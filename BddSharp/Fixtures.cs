835af6fa58fdf6056f6e9df7a6bcc08c66c52fc3
Mishkin Faustini <mfaustini@truecar.com>
2013-05-31 17:19:46 -0700

    Added EntityFramework reference and Fixture code

---
+++ BddSharp/Fixtures.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+
+namespace BddSharp
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class FixtureLoaderAttribute : Attribute
+    {
+    }
+
+    public class Fixtures : DynamicDictionary<Object>
+    {
+        public void Load(DbContext context)
+        {
+            // Loads all methods with Attribute "FixtureLoader"
+            Assembly callingAssembly = Assembly.GetCallingAssembly();
+            var members = callingAssembly.GetTypes()
+                .SelectMany(x => x.GetMembers())
+                .Union(callingAssembly.GetTypes())
+                .Where(x => Attribute.IsDefined(x, typeof(FixtureLoaderAttribute)));
+
+            foreach (var m in members)
+            {
+                m.ReflectedType.GetMethod(m.Name, BindingFlags.Public | BindingFlags.Static).Invoke(null, new object[] { this, context });
+            }
+
+            context.SaveChanges();
+        }
+
+        public void Load(DbContext context, Assembly assembly)
+        {
+            // Loads all methods with Attribute "FixtureLoader"
+            var members = assembly.GetTypes()
+                .SelectMany(x => x.GetMembers())
+                .Union(assembly.GetTypes())
+                .Where(x => Attribute.IsDefined(x, typeof(FixtureLoaderAttribute)));
+
+            foreach (var m in members)
+            {
+                m.ReflectedType.GetMethod(m.Name, BindingFlags.Public | BindingFlags.Static).Invoke(null, new object[] { this, context });
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
